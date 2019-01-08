286c6cbeb1aced5f3d6c15cdf9e4642e17c42f4e
NaabZer <anton.gefvert@gmail.com>
2019-01-08 02:25:15 +0100

    Fix problem with header on landscape phones

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -1,11 +1,14 @@
 import React, { Component, forwardRef } from 'react';
 
+import { TopAppBarFixedAdjust } from '@rmwc/top-app-bar';
+
 class PageHeader extends Component{
 
   render() {
     return(
       <React.Fragment>
         <div className='page-header'>
+          <TopAppBarFixedAdjust/>
           <div className="page-header-image"/>
           <div className='page-header-content'>
             <div className='page-header-title'>
