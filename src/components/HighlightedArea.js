b35088066e9efa3ca6390efc57cb350b22c4178c
NaabZer <anton.gefvert@gmail.com>
2019-01-16 23:43:29 +0100

    Add base sof colors to highlighted area

--- src/components/HighlightedArea.js
+++ src/components/HighlightedArea.js
@@ -5,9 +5,15 @@ import { Grid, GridCell, GridInner } from '@rmwc/grid';
 class HighlightedArea extends Component {
 
   render(){
+    var areaCname = 'highlighted-area-red';
+    if(this.props.color === 'green'){
+      areaCname = 'highlighted-area-green';
+    }else if(this.props.color ==='yellow'){
+      areaCname = 'highlighted-area-yellow';
+    }
     return(
       <React.Fragment>
-        <Grid className='highlighted-area' >
+        <Grid className={areaCname} >
           <GridInner className={['highlighted-area-inner', this.props.className].join(' ')}>
             {this.props.children}
           </GridInner>
