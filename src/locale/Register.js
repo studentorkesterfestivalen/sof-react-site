58876fac78cb99c7d933b618ba67e10b8d9b5949
Eric Dahlgren <erida597@student.liu.se>
2019-02-02 14:37:49 +0100

    Added locale file for register

---
+++ src/locale/Register.js
@@ -0,0 +1,24 @@
+export default {
+  'sv': {
+    'Register.Username': "Användarnamn",
+    'Register.Email': "Email",
+    'Register.Pass': "Lösenord",
+    'Register.PassConfirm': "Återge lösenord",
+    'Register.Register': "Registrera dig",
+    'Register.UsernameRequired' : 'Användarnamn krävs',
+    'Register.PasswordRequired' : 'Lösenord krävs',
+    'Register.PasswordDoesNotMatch' : 'Lösenorden matchar ej',
+    
+  },
+
+  'en' : {
+    'Register.Username': "Username",
+    'Register.Email': "Email",
+    'Register.Pass': "Password",
+    'Register.PassConfirm': "Confirm password",
+    'Register.Register': "Register",
+    'Register.UsernameRequired' : 'Username required',
+    'Register.PasswordRequired' : 'Password required',
+    'Register.PasswordDoesNotMatch' : 'Passwords do not match',
+  }
+};
