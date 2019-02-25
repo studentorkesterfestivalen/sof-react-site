1ee4092059d95003f1ef236adc2b30fd414ae611
NaabZer <anton.gefvert@gmail.com>
2019-02-25 03:33:44 +0100

    Add email confirm dialog when registering

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -10,8 +10,9 @@ import { Formik, Form } from 'formik';
 
 import * as Yup from 'yup';
 
-import { FormattedMessage } from 'react-intl';
+import { FormattedMessage, injectIntl } from 'react-intl';
 import { registerUser } from '../redux-token-auth-config';
+import { openDialog} from '../actions/dialog';
 
 import { connect } from 'react-redux';
 import Register from '../locale/Register';
@@ -39,15 +40,16 @@ class RegisterForm extends Component{
 
     registerUser({ email, displayName: username, password, passwordConfirmation, confirmSuccessUrl })
       .then( (response) => {
-        console.log("Du är registrerad");
-        console.log(response);
         bag.setSubmitting(false);
+        this.props.openDialog(
+          this.props.intl.formatMessage({id: 'Register.welcome'}),
+          this.props.intl.formatMessage({id: 'Register.confirmEmail1'})
+          + email 
+          + this.props.intl.formatMessage({id: 'Register.confirmEmail2'})
+        );
       } )
       .catch( (error) => {
-        console.log("BinBangbom krasch");
-        //console.log(error.response.data.errors);
-        // if(typeerror.response.data.errors)
-
+        console.log(error);
         let errors = {};
         for (let key in error.response.data.errors) {
           errors[key] = error.response.data.errors[key][0]; // for now only take the first error of the array
@@ -56,7 +58,6 @@ class RegisterForm extends Component{
         bag.setErrors( errors );
 
         bag.setSubmitting(false);
-        this.setState({error : error.response.data.errors[0] });
       } )
    }
 
@@ -66,10 +67,10 @@ class RegisterForm extends Component{
               <Formik
                 initialValues={{email: '', username: '', password: '', password_conf: ''}}
                 validationSchema={Yup.object().shape({
-                  email: Yup.string().required(<FormattedMessage id='Login.EmailRequired' />)
+                  email: Yup.string().required(<FormattedMessage id='Register.EmailRequired' />)
                   .email(<FormattedMessage id='Register.MustBeEmail' />),
-                  username: Yup.string().required(<FormattedMessage id='Login.UsernameRequired' />),
-                  password: Yup.string().required(<FormattedMessage id='Login.PasswordRequired' />),
+                  username: Yup.string().required(<FormattedMessage id='Register.usernameRequired' />),
+                  password: Yup.string().required(<FormattedMessage id='Register.PasswordRequired' />),
                   password_conf: Yup.string()
                   .oneOf([Yup.ref("password"), null], <FormattedMessage id='Register.PasswordConfirmWrong' />)
                   .required(<FormattedMessage id='Register.PasswordConfirmRequired'/>)
@@ -82,7 +83,7 @@ class RegisterForm extends Component{
                       <GridCell desktop='12' tablet='8' phone='4'>
                         <FormTextInput
                           name='email'
-                          label={<FormattedMessage id='Login.Email'/>}
+                          label={<FormattedMessage id='Register.Email'/>}
                           value={values.email}
                           error={errors.email}
                           touched={touched.email}
@@ -93,7 +94,7 @@ class RegisterForm extends Component{
                       <GridCell desktop='12' tablet='8' phone='4'>
                         <FormTextInput
                           name='username'
-                          label={<FormattedMessage id='Login.Username'/>}
+                          label={<FormattedMessage id='Register.username'/>}
                           value={values.username}
                           error={errors.username}
                           touched={touched.username}
@@ -105,7 +106,7 @@ class RegisterForm extends Component{
                         <FormTextInput
                           name='password'
                           type='password'
-                          label={<FormattedMessage id='Login.Pass'/>}
+                          label={<FormattedMessage id='Register.Pass'/>}
                           value={values.password}
                           error={errors.password}
                           touched={touched.password}
@@ -117,7 +118,7 @@ class RegisterForm extends Component{
                         <FormTextInput
                           name='password_conf'
                           type='password'
-                          label={<FormattedMessage id='Login.PassConfirm'/>}
+                          label={<FormattedMessage id='Register.PassConfirm'/>}
                           value={values.password_conf}
                           error={errors.password_conf}
                           touched={touched.password_conf}
@@ -139,4 +140,4 @@ class RegisterForm extends Component{
   }
 }
 
-export default connect(null, { registerUser })(RegisterForm)
+export default injectIntl(connect(null, { openDialog, registerUser })(RegisterForm));
--- src/locale/Register.js
+++ src/locale/Register.js
@@ -2,6 +2,8 @@ export default {
   'sv': {
     'Register.Username': "E-postadress",
     'Register.Email': "E-mail",
+    'Register.username': "Namn/Smeknamn",
+    'Register.usernameRequired': "Namn/Smeknamn krävs",
     'Register.Pass': "Lösenord",
     'Register.PassConfirm': "Bekräfta lösenord",
     'Register.Register': "Registrera dig",
@@ -10,13 +12,17 @@ export default {
     'Register.PasswordConfirmRequired' : 'Lösenordsbekräftning krävs',
     'Register.PasswordConfirmWrong' : 'Lösenorden matchar ej',
     'Register.MustBeEmail': 'Inte en korrekt mailadress',
-    'Register.PasswordConfirmRequired' : 'Lösenorden matchar ej',
-
+    'Register.back': 'Tillbaka',
+    'Register.welcome': 'Välkommen!',
+    'Register.confirmEmail1': 'Ett verifikationsemail har skickats till ',
+    'Register.confirmEmail2': ', om du inte verifierar din email inom två dagar slutar ditt konto att fungera tills den är verifierad. Du måste också verifiera din email för att kunna anmäla dig till en orkester'
   },
 
   'en' : {
     'Register.Username': "E-mail",
     'Register.Email': "Email",
+    'Register.username': "Name/Nickname",
+    'Register.usernameRequired': "Name/Nickname required",
     'Register.Pass': "Password",
     'Register.PassConfirm': "Confirm password",
     'Register.Register': "Register",
@@ -25,6 +31,9 @@ export default {
     'Register.PasswordConfirmRequired' : 'Passwords confirmation required',
     'Register.PasswordConfirmWrong' : 'Passwords do not match',
     'Register.MustBeEmail': 'Not a correct email address',
-    'Register.back': 'Back'
+    'Register.back': 'Back',
+    'Register.welcome': 'Welcome!',
+    'Register.confirmEmail1': 'A confirmation email was sent to ',
+    'Register.confirmEmail2': ', if you don\'t confirm your email withing two days, you account will stop working until you confirm your email. You also have to confirm your email to be able to register for an orchestra'
   }
 };
