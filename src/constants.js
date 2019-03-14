e6b6e240733c5c1f5a39e5e84ca0bc4914f766d0
NaabZer <anton.gefvert@gmail.com>
2019-03-14 17:32:10 +0100

    Make pass redirect constant work

--- src/constants.js
+++ src/constants.js
@@ -3,6 +3,6 @@ import runtimeEnv from '@mars/heroku-js-runtime-env';
 const env = runtimeEnv();
 
 export const authUrl = env.REACT_APP_API_ENDPOINT + "auth";
-export const changePassRedirect = process.env.PUBLIC_URL + '/account/newpass'
+export const changePassRedirect = env.URL + '/account/reset_password/new'
 
 //export default Constants;
