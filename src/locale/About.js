604ecd2779db1f42130279ed09905089220063e1
NaabZer <anton.gefvert@gmail.com>
2019-03-21 16:18:15 +0100

    Fix some text

--- src/locale/About.js
+++ src/locale/About.js
@@ -8,7 +8,7 @@ export default {
     'About.orchestra': "Orkester",
     'About.aboutText' : "9-11 maj 2019 är det återigen dags för Studentorkesterfestivalen. SOF är ett tredagarsevenemang som arrangeras av LinTek och hålls varannatår i Linköping för både studenter och icke-studenter. Dessa tre fullspäckade SOF-dagar är fyllda av underhållning i form av en kårtege, ett festivalområde och orkesterspelningar.",
     'About.festivalInfo': 'Festivalen besöks under SOF av tiotusentals studenter och består av ett område fyllt av skojiga aktiviteter, feta orkesterspelningar, servering av mat och dryck samt tre hejdundrande fester!',
-    'About.kartegeInfo': 'kårtegen som rullar genom Linköping den 11 maj har upp emot 50 000 åskådare och består av ekipage byggda av Linköpings påhittiga studenter.',
+    'About.kartegeInfo': 'Kårtegen som rullar genom Linköping den 11 maj har upp emot 50 000 åskådare och består av ekipage byggda av Linköpings påhittiga studenter.',
     'About.orchestraInfo': 'SOF får besök av hundratals orkestermedlemmar från hela vårt avlånga land, resterande länder i norden och delar av norra Europa. Orkestrarna spelar under hela SOF-helgen både på festivalområdet och nere på stan!'
   },
 
