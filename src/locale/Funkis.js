41c36ced8d8f97cdd309f0d4132d405ca9103f37
NaabZer <anton.gefvert@gmail.com>
2019-03-24 16:49:36 +0100

    Work on funkis page

--- src/locale/Funkis.js
+++ src/locale/Funkis.js
@@ -5,7 +5,11 @@ export default {
     'Funkis.p1': 'SOF19 börjar närma sig och just nu söker vi efter dig som vill ta chansen att vara med och verkställa Sveriges största studentarrangerade festival - Studentorkesterfestivalen! SOF är en tredagarsfestival fylld med musik, dans och skoj med över 10 000 unika besökare. Den 9-11 maj är det äntligen dags igen!',
     'Funkis.p2': 'Vad är funkis för något? Funkis är du som jobbar på SOF under festivalen med områdets uppbyggnad eller nedrivning, på själva festivalen med olika spännande aktiviteter, med vår kårtege som rullar genom stan eller med de fantastiska orkestrar som kommer på besök. Vi söker studenter från samtliga program och fakulteter till våra funkispass, alla är välkomna! För dig som jobbar som funkis erbjuder vi i år finfina förmåner i form av bland annat gratisbiljetter! Vilket pass du jobbar bestämmer hur många biljetter du får.',
     'Funkis.p3': 'Vill du bli en del av SOF19?  Sök funkis mellan 25/3 och 14/4!  Fr.o.m 25/3 lägg till: "Tvivla inte, sök SOF-funkis på: *länk* Tagga SOF!" OBS. Det är först till kvarn som gäller på anmälningarna till passen, så hugg ditt pass fort!',
-    'Funkis.p4': 'Har du några frågor kommer SOF-organisationen finnas på plats i Colosseum, Key och US under vecka 13-15 för att besvara era frågor och ta emot anmälningar! Annars kan ni kontakta någon av nedanstående personer'
+    'Funkis.p4': 'Har du några frågor kommer SOF-organisationen finnas på plats i Colosseum, Key och US under vecka 13-15 för att besvara era frågor och ta emot anmälningar! Annars kan ni kontakta någon av nedanstående personer',
+    'Funkis.t2': 'Förmåner',
+    'Funkis.p5': 'När du jobbar som funkis så får du så klart en del förmåner. Förutom ett exklusivt funkismärke, en supersnygg funkis t-shirt och en massa härliga, nya vänner så kommer man som funkis få gratis inträde till vissa av festivalens kvällar beroende på när och med vad du arbetar.',
+    'Funkis.timeLeft': 'Tid kvar tills funkisanmälan stänger',
+    'Funkis.register': 'Klicka här för att anmäla dig',
   },
 
   'en' : {
--- src/pages/Funkis.js
+++ src/pages/Funkis.js
@@ -4,15 +4,32 @@ import HighlightedArea from '../components/HighlightedArea';
 import SofCountdown from '../components/SofCountdown'
 import { Grid, GridCell, GridInner } from '@rmwc/grid';
 import ContactCard from '../components/ContactCard';
+import { ListDivider } from '@rmwc/list';
+
+import posed from 'react-pose';
 
 const contactEmilia = {name: 'Emilia Edman', title: 'Personal', email: 'personal', image:'https://s3-eu-west-1.amazonaws.com/lintek-sof/sof-react-page/Pictures/Committee_Profile/hejsan.jpg'};
 const contactSofia = {name: 'Sofia Hagel', title: 'Samordnare Kommunikation', email: 'kommunikation', image:'https://s3-eu-west-1.amazonaws.com/lintek-sof/sof-react-page/Pictures/Committee_Profile/aappelknyckaren.jpg'};
+
+
+const PosedForm = posed.div({
+  open: {
+    height: '1400px',
+    applyAtStart:{display: 'initial', overflow: 'hidden'},
+  },
+  closed: {
+    height: '0px',
+    applyAtEnd:{display: 'none'},
+  },
+});
 
 class Funkis extends Component{
 
   constructor(props) {
     super(props);
     this.intl = this.props.intl;
+
+    this.state = {formOpen: false}
   };
 
   static pageTitle(){
@@ -41,6 +58,54 @@ class Funkis extends Component{
               <p>
                 <FormattedMessage id='Funkis.p3' />
               </p>
+            </GridCell>
+        </Grid>
+        <HighlightedArea className='countdown-inner' color='green'
+        >
+          <SofCountdown 
+            label={<FormattedMessage id='Funkis.timeLeft' />}
+            toDate={new Date('2019-05-09T08:00:00')} />
+          <GridCell phone='4' tablet='8' desktop='12' >
+            <ListDivider/>
+          </GridCell>
+          <GridCell phone="4" tablet="8" desktop='12' className = 'h-center'>
+            <h4 style={{margin: '10px'}}>
+              <b onClick={() => this.setState({formOpen: !this.state.formOpen})} style={{color: 'white', cursor: 'pointer'}} >
+                <FormattedMessage id='Funkis.register' /> 
+              </b>
+            </h4>
+          </GridCell>
+          <GridCell phone="4" tablet="8" desktop='12' className = 'h-center' style={{height: '100%'}}>
+            <PosedForm 
+              style={{width: '100%'}}
+              pose={this.state.formOpen? 'open' : 'closed'}
+            >
+              <iframe 
+                src="https://docs.google.com/forms/d/e/1FAIpQLScOcFdNFkMw1wffnmyhzhRAVFPxkLVyckPPBp6TZNQ143Bnkw/viewform?embedded=true" 
+                style={{height: '100%', width: '100%'}}
+                frameBorder="0" 
+                marginHeight="0" 
+                marginWidth="0"
+              >
+                  Läser in...
+              </iframe>
+            </PosedForm>
+          </GridCell>
+          {/*(!this.state.timerFinished) ?
+              <GridCell span='12'>
+                <Button
+                  raised
+                  style={{width: '100%'}}
+                  onClick={() => this.setState({toDate: new Date(Date.now() + 2000)})} 
+                > 
+                  Press to test timer 
+                </Button>
+              </GridCell>
+              : ''
+              */}
+        </HighlightedArea>
+        <Grid className="base-outer-grid ">
+            <GridCell phone="4" tablet="8" desktop='12'>
               <p>
                 <FormattedMessage id='Funkis.p4' />
               </p>
@@ -62,6 +127,14 @@ class Funkis extends Component{
                 image={contactSofia.image}
                 clickable
               />
+            </GridCell>
+            <GridCell phone="4" tablet="8" desktop='12'>
+              <h2>
+                <FormattedMessage id='Funkis.t2' />
+              </h2>
+              <p>
+                <FormattedMessage id='Funkis.p5' />
+              </p>
             </GridCell>
         </Grid>
 
