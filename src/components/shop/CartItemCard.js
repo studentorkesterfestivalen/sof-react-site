a8d428e6c31812aca4d0ee3016f299f6fc2e0eaf
NaabZer <anton.gefvert@gmail.com>
2019-04-22 12:09:06 +0200

    Make cart cards work with actual products

--- src/components/shop/CartItemCard.js
+++ src/components/shop/CartItemCard.js
@@ -4,30 +4,21 @@ import {
   Card,
   CardMedia,
 } from '@rmwc/card';
-
-import { Ripple } from '@rmwc/ripple';
-
 import {
   List,
   ListItem,
   ListItemText,
   ListItemPrimaryText,
-  ListItemSecondaryText,
-  ListItemGraphic,
+  ListItemSecondaryText
 } from '@rmwc/list';
 import { TextField } from '@rmwc/textfield';
 import { IconButton } from '@rmwc/icon-button';
-import { FormattedMessage, injectIntl } from 'react-intl'
 
-import posed from 'react-pose/lib/index';
+import { injectIntl } from 'react-intl'
 
-const test_articles = [
-  {name: 'Biljett - Torsdag', description: 'hi hello my name is a text that explains this product', cost: 1337, id: 0, imageURL:'https://www.eventwristbands.com/images/products/7616.png' },
-  {name: 'Biljett - Fredag', description: 'Hejsan hoppsan en sådan grej, hipp hurra och hej', cost: 42, id: 1, imageURL: 'https://www.wristband.com/getmedia/00c5dd96-7d96-4330-a1cf-a91ffe02fd05/tyvek.png.aspx'},
-  {name: 'Biljett - Lördag', description: 'Tagga SOF!', cost: 9001, id: 2, imageURL: 'https://images-na.ssl-images-amazon.com/images/I/615mvW2E03L._SX425_.jpg'},
-  {name: 'Biljett - Helhelg', description: 'bing bing bong', cost: 13, id: 3, imageURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCmVgAlNz8TvpImvL_-UusOPUtbM-ym-o0vcp2V0fQFDjeYpXQSQ'},
-  {name: 'Märke', description: 'Visa bandeeen', cost: 3.14, id: 4, imageURL: 'https://cdn.shopify.com/s/files/1/1275/7969/products/pretty-useful-co-send-noods-patch_1024x.jpg?v=1517845970'},
-]
+import posed from 'react-pose';
+
+import { connect } from 'react-redux';
 
 class CardItemCard extends Component{
   constructor(props){
@@ -53,33 +44,60 @@ class CardItemCard extends Component{
   }
 
   render(){
-    console.log(this.props.item);
-    const {prodID, quantity} = this.props.item;
-    const item = test_articles[prodID];
+    const {prodID, amount} = this.props.item;
+    const baseProductIds = this.props.baseProducts[prodID];
+    const baseProduct = this.props.products[baseProductIds['base_id']];
+    const product = baseProduct.products[baseProductIds['prod_id']];
+
+    const hasTypes = baseProduct.products.length > 1;
+    const cost = hasTypes ? product.actual_cost : baseProduct.cost;
     return(
       <React.Fragment>
         <Card 
           className='cart-item-card' 
         >
-          <CardMedia
-            style={{ backgroundImage: 'url(' + item.imageURL + ')'}}
-          />
+          {(baseProduct.has_image) ?
+              <CardMedia
+                style={{ backgroundImage: 'url(' + baseProduct.image_path + ')'}}
+              /> :
+              null
+          }
           <List nonInteractive >
             <ListItem ripple={false} style={{overflow: 'visible'}}>
               <ListItemText>
-                {item.name}
+                {(hasTypes) ?
+                  <React.Fragment>
+                    <ListItemPrimaryText>
+                      <b>
+                        {baseProduct.name}
+                      </b>
+                    </ListItemPrimaryText>
+                    <ListItemSecondaryText>
+                      {product.kind}
+                    </ListItemSecondaryText>
+                  </React.Fragment>
+                  :
+                  <b>{baseProduct.name}</b>
+                }
               </ListItemText>
             </ListItem>
           </List>
           <IconButton icon='remove' onClick={() => this.remove(prodID)} />
-          <TextField outlined value={quantity}
+          <TextField outlined value={amount}
             onChange={(e) => this.handleChange(prodID, e.target)} label='Mängd'
           />
           <IconButton icon='add' onClick={() => this.add(prodID)} />
-          <List nonInteractive >
+          <List nonInteractive twoLine>
             <ListItem ripple={false} style={{overflow: 'visible'}}>
               <ListItemText>
-                  {item.cost + ' Kr'}
+                <ListItemPrimaryText>
+                  <b>
+                    {cost + this.props.intl.locale === 'sv' ? ' Kr' : " SEK"}
+                  </b>
+                </ListItemPrimaryText>
+                <ListItemSecondaryText style={{color: '#F00'}}>
+                  1000+ kvar
+                </ListItemSecondaryText>
               </ListItemText>
             </ListItem>
           </List>
@@ -90,5 +108,11 @@ class CardItemCard extends Component{
   }
 }
 
-export default injectIntl(CardItemCard);
+const mapStateToProps = state => ({
+  products: state.shop.products,
+  baseProducts: state.shop.base_products,
+  isLoading: state.shop.loading
+});
+
+export default connect(mapStateToProps)(injectIntl(CardItemCard));
 
