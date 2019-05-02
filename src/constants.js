fff966b0e9fe4a7f1db746ae47bd21aa16a8b9f1
NaabZer <anton.gefvert@gmail.com>
2019-05-02 12:42:27 +0200

    Fix constants

--- src/constants.js
+++ src/constants.js
@@ -5,6 +5,6 @@ const env = runtimeEnv();
 export const authUrl = env.REACT_APP_API_ENDPOINT + '/auth';
 export const frontEndPath = env.REACT_APP_URL;
 export const changePassRedirect = frontEndPath + '/account/reset_password/new';
-export const stripePublicKey = env.REACT_STRIPE_PUBLIC_KEY;
+export const stripePublicKey = env.REACT_APP_STRIPE_PUBLIC_KEY;
 
 //export default Constants;
