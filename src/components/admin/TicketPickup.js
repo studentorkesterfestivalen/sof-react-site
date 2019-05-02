5273a2ebfbccb5c766a22b2028b06c94d36c237c
Mathias Berggren <mathiasberggren1@gmail.com>
2019-05-02 16:09:24 +0200

    Base ticket signup component

---
+++ src/components/admin/TicketPickup.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+
+
+class TicketPickup extends Component {
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return (
+      <React.Fragment>
+        HÄR SKA MASSA FINA SAKER VARA!!
+      </React.Fragment>
+    );
+  }
+
+};
+
+export default TicketPickup;
