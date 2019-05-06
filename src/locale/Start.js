dff9a42767ad8ab18e79b6b9989946d075580edd
NaabZer <anton.gefvert@gmail.com>
2019-05-06 11:23:28 +0200

    Fix typo

--- src/locale/Start.js
+++ src/locale/Start.js
@@ -6,7 +6,7 @@ export default {
     'Start.festivalTitle': "Årets festival",
     'Start.buyTicket': "Klicka här för att köpa din biljett nu!",
     'Start.moreInfo': 'Om SOF',
-    'Start.funkis': 'Vill du ha en GRATIS helhelgsbiljett (värde 400kr) samt massa andra godsaker? Då kan du söka funkis och jobba ett 6-8 timmar och få allt detta. Vill du veta mer?',
+    'Start.funkis': 'Vill du ha en GRATIS helhelgsbiljett (värde 400kr) samt massa andra godsaker? Då kan du söka funkis och jobba 6-8 timmar och få allt detta. Vill du veta mer?',
     'Start.funkisButton': 'Klicka då HÄR!',
   },
 
@@ -17,7 +17,7 @@ export default {
     'Start.festivalTitle': "This years Festival",
     'Start.buyTicket': "Click here to purchase your ticket now!",
     'Start.moreInfo': 'About SOF',
-    'Start.funkis': 'Vill du ha en GRATIS helhelgsbiljett (värde 400kr) samt massa andra godsaker? Då kan du söka funkis och jobba ett 6-8 timmar och få allt detta. Vill du veta mer?',
+    'Start.funkis': 'Vill du ha en GRATIS helhelgsbiljett (värde 400kr) samt massa andra godsaker? Då kan du söka funkis och jobba 6-8 timmar och få allt detta. Vill du veta mer?',
     'Start.funkisButton': 'Klicka då HÄR!',
   }
 };
